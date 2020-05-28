e1c42ac32343fe11ef60842d40e743f97bc88a89
mahyar-osn <m.osanlouy@auckland.ac.nz>
2020-05-29 08:52:27 +1200

    updated the code into app

--- point_cloud_resampling/src/point_cloud_resampling/model/master.py
+++ point_cloud_resampling/src/point_cloud_resampling/model/master.py
@@ -1,5 +1,153 @@
+import os
 
-class MasterModel(object):
+import pandas as pd
+import numpy as np
+import math
+import rtree
+import open3d as o3d
+
+
+class PointCloudResampling(object):
 
     def __init__(self):
-        return
+        self._input_filename = None
+        self._output_filename = None
+        self._factor = None
+        self._output_directory = None
+
+        self._pcd = None
+        self._pcd_array_temp = None
+        self._pcd_array_unique = None
+        self._pcd_array_resampled = None
+        self._pcd_resampled = None
+
+        self._original_max = None
+        self._original_min = None
+        self._original_size = None
+        self._resampled_max = None
+        self._resampled_min = None
+        self._resampled_size = None
+
+    def set_input_file(self, input_file):
+        self._input_filename = input_file
+
+    def set_output_location(self, output_location):
+        self._output_directory = output_location
+
+    def set_factor(self, factor):
+        self._factor = int(factor)
+
+    def get_original_size(self):
+        return self._original_size
+
+    def get_original_max(self):
+        return self._original_max
+
+    def get_original_min(self):
+        return self._original_min
+
+    def get_resampled_size(self):
+        return self._resampled_size
+
+    def get_resampled_max(self):
+        return self._resampled_max
+
+    def get_resampled_min(self):
+        return self._resampled_min
+
+    def get_result_report(self):
+        index = ['Original', 'Resampled']
+        column = ['Size', 'Min', 'Max']
+        data = np.asarray([
+            [self._original_size, self._original_min, self._original_max],
+            [self._resampled_size, self._resampled_min, self._resampled_max]
+        ])
+        result_array = pd.DataFrame(data, index=index, columns=column)
+        result_array.to_csv(os.path.join(self._output_directory, 'Resampled_Report.csv'), sep=',')
+
+    def _check_duplicate_rows(self):
+        print("Removing duplicate rows and saving the new txt file...")
+        self._pcd_array_unique = np.unique(self._pcd_array_temp.values, axis=0)
+        filename, file_extension = os.path.basename(self._input_filename).split('.')
+        output_unique_file = filename + "_unique." + file_extension
+        np.savetxt(os.path.join(self._output_directory, output_unique_file),
+                   self._pcd_array_unique,
+                   delimiter=" ",
+                   fmt="%5f")
+        self._pcd_unique = o3d.io.read_point_cloud(os.path.join(self._output_directory, output_unique_file),
+                                                   format="xyz")
+
+        self._original_size = self._pcd_array_unique.shape[0]
+        self._original_min = self._pcd_array_unique[:, 2].min()
+        self._original_max = self._pcd_array_unique[:, 2].max()
+        print("")
+
+    @staticmethod
+    def dist(p, q):
+        """
+        Return the Euclidean distance between points p and q.
+        """
+        return math.hypot(p[0] - q[0], p[1] - q[1])
+
+    def _local_resample(self):
+
+        print("")
+        print("Local resampling. This may take a while...")
+
+        r = self._factor
+
+        data = self._pcd_array_unique
+
+        result = []
+        index = rtree.index.Index()
+
+        for i, p in enumerate(data):
+            px, py, pz = p
+            nearby = index.intersection((px - r, py - r, px + r, py + r))
+            if all(self.dist(p, data[j]) >= r for j in nearby):
+                result.append(p)
+                index.insert(i, (px, py, px, py))
+
+        resampled_array = np.asarray(result)
+        self._resampled_size = resampled_array.shape[0]
+        self._resampled_min = resampled_array[:, 2].min()
+        self._resampled_max = resampled_array[:, 2].max()
+
+        return resampled_array
+
+    def _load_file(self):
+        if os.path.exists(self._input_filename):
+            print("")
+            print("Loading txt file...")
+            self._pcd = o3d.io.read_point_cloud(self._input_filename, format="xyz")
+            self._pcd_array_temp = pd.read_csv(self._input_filename, header=None, sep=" ")
+        else:
+            raise FileNotFoundError("Input txt file not found! Please check the path and try again.")
+        print("Txt file loaded! ")
+        print("")
+        self._check_duplicate_rows()
+
+    def resample(self):
+        self._load_file()
+        self._pcd_array_resampled = self._local_resample()
+        self._save_resampled_file()
+
+    def _save_resampled_file(self):
+        print("Saving the resampled file. This may take a while...")
+        filename, file_extension = os.path.basename(self._input_filename).split('.')
+        output_unique_file = filename + "_resampled_at_{}_factor.".format(str(self._factor)) + file_extension
+        np.savetxt(os.path.join(self._output_directory, output_unique_file),
+                   self._pcd_array_resampled,
+                   delimiter=" ",
+                   fmt="%5f")
+
+        self._pcd_resampled = o3d.io.read_point_cloud(os.path.join(self._output_directory, output_unique_file),
+                                                      format="xyz")
+
+        print("Saved!")
+
+    def view_original(self):
+        o3d.visualization.draw_geometries([self._pcd_unique])
+
+    def view_resampled(self):
+        o3d.visualization.draw_geometries([self._pcd_resampled])
--- point_cloud_resampling/src/point_cloud_resampling/view/resampling_app_widget.py
+++ point_cloud_resampling/src/point_cloud_resampling/view/resampling_app_widget.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 from PySide import QtGui
 
 from .ui_resampling_app import Ui_MainWindow
+from ..model.master import PointCloudResampling
 
 
 class ResamplingWidget(QtGui.QWidget):
@@ -8,8 +11,19 @@ class ResamplingWidget(QtGui.QWidget):
     def __init__(self, parent=None):
         super(ResamplingWidget, self).__init__(parent)
 
+        self._model = PointCloudResampling()
+
         self._ui = Ui_MainWindow()
         self._ui.setupUi(self)
+        self._check_settings = dict(input_file=False, output_file=False, factor=False)
+        self._previous_location = ''
+
+        self._ui.date_label.setText("Date: " + datetime.today().strftime('%d-%m-%Y'))
+
+        self._ui.plotResampled_pushButton.setEnabled(False)
+        self._ui.plotOriginal_pushButton.setEnabled(False)
+        self._ui.run_pushButton.setEnabled(False)
+        self._ui.export_pushButton.setEnabled(False)
 
         self._source_path = None
 
@@ -17,34 +31,69 @@ class ResamplingWidget(QtGui.QWidget):
         self.showNormal()
 
     def _make_connections(self):
-        self._ui.open_pushButton.clicked.connect(self._select_source)
-        self._ui.save.clicked.connect(self._save)
+        self._ui.load_pushButton.clicked.connect(self._select_source)
+        self._ui.output_pushButton.clicked.connect(self._select_output_location)
+        self._ui.doubleSpinBox.valueChanged.connect(self._factor_changed)
+        self._ui.run_pushButton.clicked.connect(self._run)
+        self._ui.plotResampled_pushButton.clicked.connect(self._plot_resampled)
+        self._ui.plotOriginal_pushButton.clicked.connect(self._plot_original)
+        self._ui.export_pushButton.clicked.connect(self._export_report)
 
     def _select_source(self):
-        self._source_path = str(QtGui.QFileDialog.getExistingDirectory(self, "Select Source"))
+        self._source_path = QtGui.QFileDialog.getOpenFileName(self, 'Select File Location', self._previous_location)
 
         if not self._source_path:
             QtGui.QMessageBox.warning(self, 'Invalid source file.', 'Please select a valid source.')
             return
 
-        # display path
-        self._ui.source_path.setText(self._source_path)
+        # set input in the model
+        if self._source_path:
+            self._previous_location = self._source_path[0]
+        self._model.set_input_file(self._previous_location)
+        self._check_settings["input_file"] = True
+        self._check_run()
 
-    def _save(self):
-        self._ui.statusbar.showMessage('Saving...', 10000)
+    def _select_output_location(self):
+        self._output_path = str(QtGui.QFileDialog.getExistingDirectory(self, "Select Output Location:"))
 
-        # check source path
-        if not self._source_path:
-            QtGui.QMessageBox.warning(self, 'Invalid source file', 'Please select a valid source')
+        if not self._output_path:
+            QtGui.QMessageBox.warning(self, 'Invalid output file.', 'Please select a valid path.')
             return
 
-        # get save path
-        save_path = str(QtGui.QFileDialog.getExistingDirectory(self, "Select a destination"))
-        if not save_path:
-            QtGui.QMessageBox.warning(self, 'Invalid destination.', 'Please select a valid folder.')
-            return
-        save_path = self._update_save_path(save_path)
+        # set output in the model
+        self._model.set_output_location(self._output_path)
+        self._check_settings["output_file"] = True
+        self._check_run()
 
-        self._grid(save_path)
+    def _factor_changed(self):
+        value = self._ui.doubleSpinBox.value()
+        self._model.set_factor(value)
+        self._check_settings["factor"] = True
+        self._check_run()
 
-        self._ui.statusbar.showMessage('Resampled data saved.', 5000)
+    def _check_run(self):
+        if list(self._check_settings.values()) == [True, True, True]:
+            self._ui.run_pushButton.setEnabled(True)
+
+    def _run(self):
+        self._ui.statusbar.showMessage('Loading file...', 10000)
+        self._model.resample()
+        self._ui.plotResampled_pushButton.setEnabled(True)
+        self._ui.plotOriginal_pushButton.setEnabled(True)
+
+        self._ui.origSampleSize_label.setText("Original Sample Size: {}".format(self._model.get_original_size()))
+        self._ui.resSampleSize_label.setText("Resampled Sample Size: {}".format(self._model.get_resampled_size()))
+        self._ui.origMinMax_label.setText("Original Min/Max: {0}/{1}".format(self._model.get_original_min(),
+                                                                             self._model.get_original_max()))
+        self._ui.resMinMax_label.setText("Resampled Min/Max: {0}/{1}".format(self._model.get_resampled_min(),
+                                                                             self._model.get_resampled_max()))
+        self._ui.export_pushButton.setEnabled(True)
+
+    def _plot_resampled(self):
+        self._model.view_resampled()
+
+    def _plot_original(self):
+        self._model.view_original()
+
+    def _export_report(self):
+        self._model.get_result_report()
